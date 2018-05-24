466fa035f663769d27579436522139b2350ef7eb
Sapein <sapeint@hashbang.sh>
2018-05-23 21:07:46 -0400

    Finish up work on setting up the websub program

--- src/twitch_api/listen.py
+++ src/twitch_api/listen.py
@@ -1,36 +1,54 @@
+import config
 import http.server
 import urllib.parse
+import urllib.request
+import hashlib
+import struct
 import sys
 import time
 import json
 
-class TwitchServer(http.server.HTTPServer):
-    def __init__(self, server_address, RequestHandlerClass, challenge, bind_and_activate=True):
-        self.challenge=challenge
-        super().__init__(server_address, RequestHandlerClass, bind_and_activate)
-
-    def finish_request(self, request, client_address):
-        self.RequestHandlerClass(request, client_address, self, self.challenge)
-
 class HookHandler(http.server.BaseHTTPRequestHandler):
     server_version = "HookHandler/0.1"
-
-    def __init__(self, request, client_address, server, challenge=None):
-        self.challenge=challenge
-        super().__init__(request, client_address, server)
 
     def do_GET(s):
         challenge_token = s.requestline.split('challenge=')[1].split('&')[0]
         s.send_response(200, challenge_token.encode('utf-8'))
 
     def do_POST(s):
-        print(s.headers)
+        signature = s.headers['X-Hub-Signature']
         length = int(s.headers['Content-Length'])
         post_data = s.rfile.read(length).decode("utf-8")
-        print("data")
-        print(post_data)
-        print("data")
-        s.send_response(200, self.challenge.encode('utf-8'))
+        if signature = hashlib.sha256(post_data).hexdigest():
+            print("data")
+            decoded_data = json.load(post_data)
+            header = {'Authorization':'Bearer {token}'.format(token=config.app_key)}
+            url="https://api.twitch.tv/helix/users?login={login_name}".format(login_name=config.twitch_username)
+            req = urllib.request.Request(url, headers=header)
+            try:
+                with urllib.request.urlopen(req, context=ssl.SSLContext()) as page:
+                    data = json.loads(page.read().decode('utf-8'))
+            except urllib.error.HTTPError as e:
+                print(e.code)
+                print(e.read())
+                raise
+            uid = data['data'][0]['display_name']
+            try:
+                req_id = data['id']
+            except KeyError:
+                try:
+                    req_id = data['data']['id']
+                except KeyError:
+                    req_id = 0
+            send_struct = struct.pack('LiLs', req_id, 0, len(uid), uid)
+            with open("{}twitch_websub".format(config.FIFO_locations), 'w') as f:
+                f.write(send_struct)
+            print("data")
+            s.send_response(200)
+        else:
+            print("Data does not hash properly!")
+            s.send_response(200)
+
 
 if __name__ == "__main__":
     server_class = http.server.HTTPServer
--- src/twitch_api/twitch_callback.py
+++ src/twitch_api/twitch_callback.py
@@ -1,20 +1,66 @@
-import urllib.request
+#! /usr/bin/python3
+import http.server
+import json
 import ssl
-import .listen
-import .config
+import struct
+import urllib.request
 
+import listen
+import config
+
+def release_key(app_key):
+    url = 'https://id.twitch.tv/oauth2/revoke?client_id={}&token={}'.format(config.twitch_client_id, app_key)
+    req = urllib.request.Request(url, method="POST")
+    try:
+        with urllib.request.urlopen(req, context=ssl.SSLContext()) as page:
+            pass
+    except urllib.error.HTTPError as e:
+        print(e.code())
+        print(e.read())
+        raise
+
+def get_twitch_id():
+    url=("https://id.twitch.tv/oauth2/token"
+         "?client_id={}"
+         "&client_secret={}"
+         "&grant_type=client_credentials").format(config.twitch_client_id, config.twitch_client_secret)
+    req = urllib.request.Request(url, method='POST')
+    try:
+        with urllib.request.urlopen(req, context=ssl.SSLContext()) as page:
+            data = json.loads(page.read().decode('utf-8'))
+    except urllib.error.HTTPError as e:
+        print(e.code)
+        print(e.read())
+        raise
+
+    app_key = data['access_token']
+    header = {'Authorization':'Bearer {token}'.format(token=app_key)}
+    url="https://api.twitch.tv/helix/users?login={login_name}".format(login_name=config.twitch_username)
+    req = urllib.request.Request(url, headers=header)
+    try:
+        with urllib.request.urlopen(req, context=ssl.SSLContext()) as page:
+            data = json.loads(page.read().decode('utf-8'))
+    except urllib.error.HTTPError as e:
+        print(e.code)
+        print(e.read())
+        raise
+    uid = data['data'][0]['id']
+    config.app_key = app_key
+    return uid
 
 def subscribe():
+    if not config.twitch_user_id:
+        config.twitch_user_id = get_twitch_id()
+        print("Your Client ID: {}".format(config.twitch_user_id))
     header={'Client-ID':config.twitch_client_id, 'Content-Type':'application/json'}
     u_data = ('{{"hub.mode":"subscribe",'
               '"hub.topic":"https://api.twitch.tv/helix/users/follows?first=1&to_id={}",'
-              '"hub.callback":"{}"}}').format(config.twitch_user_id, config.url)
+              '"hub.callback":"{}",'
+              '"hub.secret":"{}"}}').format(config.twitch_user_id, config.url, config.secret)
     dataa=u_data.encode('utf-8')
     header['Content-Length'] = len(dataa)
     header['User-Agent'] = 'twitchtest'
-    req = urllib.request.Request('https://api.twitch.tv/helix/webhooks/hub', data=dataa, headers=header, method='POST') 
-    print(req.data)
-    print(req.headers)
+    req = urllib.request.Request('https://api.twitch.tv/helix/webhooks/hub', data=dataa, headers=header, method='POST')
     ret_data = urllib.request.urlopen(req, context=ssl.SSLContext())
     ret_code = ret_data.getcode()
     if ret_code == 200:
@@ -22,10 +68,10 @@ def subscribe():
         import sys;sys.exit(1)
     elif ret_code == 202:
         print("Accepted!")
-        server_class = listen.TwitchServer
-        httpd = server_class(("", 8080), listen.HookHandler, None)
+        server_class = http.server.HTTPServer
+        httpd = server_class(("", 8080), listen.HookHandler)
         httpd.handle_request()
- 
+
 def unsubscribe():
     header={'Client-ID':config.twitch_client_id, 'Content-Type':'application/json'}
     u_data = ('{{"hub.mode":"unsubscribe",'
@@ -34,9 +80,7 @@ def unsubscribe():
     dataa=u_data.encode('utf-8')
     header['Content-Length'] = len(dataa)
     header['User-Agent'] = 'twitchtest'
-    req = urllib.request.Request('https://api.twitch.tv/helix/webhooks/hub', data=dataa, headers=header, method='POST') 
-    print(req.data)
-    print(req.headers)
+    req = urllib.request.Request('https://api.twitch.tv/helix/webhooks/hub', data=dataa, headers=header, method='POST')
     ret_data = urllib.request.urlopen(req, context=ssl.SSLContext())
     ret_code = ret_data.getcode()
     if ret_code == 200:
@@ -44,15 +88,17 @@ def unsubscribe():
         import sys;sys.exit(1)
     elif ret_code == 202:
         print("Accepted!")
-        server_class = listen.TwitchServer
-        httpd = server_class(("", 8080), listen.HookHandler, None)
+        server_class = http.server.HTTPServer
+        httpd = server_class(("", 8080), listen.HookHandler)
         httpd.handle_request()
 
 def main():
-    pass
+    with open("{}websub".format(config.FIFO_locations), 'w') as f:
+        f.write("")
+    httpd = http.server.HTTPServer(("", 8080), listen.HookHandler))
+    httpd.serve_forever()
 
 if __name__ == "__main__":
-    print("LAUNCH")
     try:
         subscribe()
         main()
@@ -60,3 +106,4 @@ if __name__ == "__main__":
         pass
     finally:
         unsubscribe()
+        release_key(config.app_key)
